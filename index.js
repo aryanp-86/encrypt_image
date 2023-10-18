7c03a5c0a17ff83b866a506dd5deeadb237526f3
aryanp86 <aryan.pokharkar21@vit.edu>
2023-10-18 15:15:13 +0530

    error solved

--- index.js
+++ index.js
@@ -11,17 +11,19 @@ var privateKeyB = eccrypto.generatePrivate();
 var publicKeyB = eccrypto.getPublic(privateKeyB);
 
 app.use(cors());
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 app.post('/encrypt-image', (req, res) => {
     // Encrypting the message for B.
-    const data = req.body;
+    const data = req.body.data;
     eccrypto.encrypt(publicKeyB, Buffer.from(`${data}`)).then(function (encrypted) {
         // B decrypting the message
         console.log(encrypted);
-        res.status(200).json({ data: encrypted });
+        res.status(200).json(encrypted);
         // eccrypto.decrypt(privateKeyB, encrypted).then(function (plaintext) {
         //     console.log("Message to part B:", plaintext.toString());
         // });
@@ -29,14 +31,24 @@ app.post('/encrypt-image', (req, res) => {
 })
 
 app.post('/decrypt-image', (req, res) => {
-    // Encrypting the message for B.
-    const dataEncrypted = req.body;
-    eccrypto.decrypt(privateKeyB, encrypted).then(function (plaintext) {
-        console.log("Decrypted image", plaintext.toString());
-        res.status(200).json({ data: dataEncryptedncrypted });
-    });
+    const dataEncrypted = {
+        iv: Buffer.from(req.body.iv.data),
+        ephemPublicKey: Buffer.from(req.body.ephemPublicKey.data),
+        ciphertext: Buffer.from(req.body.ciphertext.data),
+        mac: Buffer.from(req.body.mac.data)
+    };
 
-})
+    eccrypto.decrypt(privateKeyB, dataEncrypted)
+        .then(plaintext => {
+            console.log("Decrypted image:", plaintext.toString());
+            res.status(200).json({ data: plaintext.toString() });
+        })
+        .catch(error => {
+            console.error("Decryption failed:", error);
+            res.status(500).send("Decryption failed");
+        });
+});
+
 
 app.listen(port, () => {
     console.log(`Encryption server listening on port ${port}`)
--- server.js
+++ server.js
@@ -1,32 +1,19 @@
-
-const express = require('express')
-const crypto = require("./index.js")
-const app = express()
-const cors = require('cors');
-const port = 3001
 var eccrypto = require("eccrypto");
 
 var privateKeyB = eccrypto.generatePrivate();
 var publicKeyB = eccrypto.getPublic(privateKeyB);
 
-app.use(cors());
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+// Encrypt
+eccrypto.encrypt(publicKeyB, Buffer.from("Test message"))
+    .then(encrypted => {
+        console.log("Encrypted:", encrypted);
 
-app.post('/encrypt-image', (req, res) => {
-    // Encrypting the message for B.
-    const data = req.body;
-    eccrypto.encrypt(publicKeyB, Buffer.from(`${data}`)).then(function (encrypted) {
-        // B decrypting the message
-        console.log(encrypted);
-        res.status(200).json({ data: encrypted });
-        // eccrypto.decrypt(privateKeyB, encrypted).then(function (plaintext) {
-        //     console.log("Message to part B:", plaintext.toString());
-        // });
+        // Decrypt
+        return eccrypto.decrypt(privateKeyB, encrypted);
+    })
+    .then(decrypted => {
+        console.log("Decrypted message:", decrypted.toString());
+    })
+    .catch(err => {
+        console.error("Error:", err);
     });
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
